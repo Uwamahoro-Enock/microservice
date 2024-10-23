36447f3b3e87140c2ebf5e2530670fb61a5421e7
Euwamahoro <enockdev01@gmail.com>
2024-10-23 23:32:33 +0100

    adding a and configuring consumer

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,21 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { TodoConsumer } from './todo.consumer'; // Adjust the path as necessary
 
 @Module({
   imports: [
     ClientsModule.register([
       {
-        name:'todo',
+        name: 'todo',
         transport: Transport.GRPC,
         options: {
           protoPath: join(__dirname, '../todo.proto'),
-          package: 'todo'
-        }
-        }
-    ])
+          package: 'todo',
+        },
+      },
+      {
+        name: 'TODO_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'], 
+          queue: 'todos', //
+          queueOptions: {
+            durable: false, 
+          },
+        },
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TodoConsumer], // Add the RabbitMQ consumer service
 })
 export class AppModule {}
